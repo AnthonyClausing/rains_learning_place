792265dd0d45d0e3e3d7f4a2d238e94ace648c14
AnthonyClausing <anthonyclausing@gmail.com>
2019-02-06 17:42:44 -0500

    Setup html for login screen

---
+++ src/components/CodeScreen.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="code-screen">
+    <div class="number-group">
+    	<div v-for="num in numbersArray(1,2,3)" :key="num">
+        	<img :src="`@/src/assets/number${num}`"/>
+      	</div>
+    </div>
+    <div class="number-group">
+    	<div v-for="num in numbersArray(4,5,6)" :key="num">
+        	<img :src="`@/src/assets/number${num}`"/>
+      	</div>
+    </div>
+    <div class="number-group">
+      	<div v-for="num in numbersArray(7,8,9)" :key="num">
+        	<img :src="`@/src/assets/number${num}`"/>
+      	</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CodeScreen",
+  methods: {
+
+    numbersArray : (num1, num2, num3) => [num1,num2,num3]
+    
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
+
